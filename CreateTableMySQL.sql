5b69811a8ac7bd080746315141d7a26d3854d83b
Andy <tacticalandy4@gmail.com>
2020-06-04 15:04:59 +0800

    6/4 新增說明

---
+++ CreateTableMySQL.sql
@@ -0,0 +1,44 @@
+﻿-- 本檔案必須用UTF-8編碼存檔
+-- 請在SQLYog中執行本Script
+-- 
+-- 
+-- 
+-- 刪除資料庫jspdb，如果它已存在
+DROP DATABASE IF EXISTS jspdb;
+-- 
+-- 建立資料庫
+CREATE DATABASE IF NOT EXISTS jspdb CHARACTER SET UTF8 COLLATE utf8_unicode_ci ;
+-- 
+-- 挑選資料庫
+USE  jspdb;
+-- 建立 example 專案所需之表格
+DROP TABLE IF EXISTS  MemberExample;
+-- 
+CREATE TABLE MemberExample 
+  ( PK int auto_increment, 
+	account varchar(32), 
+	password varchar(32), 
+	name varchar(32), 
+	email varchar(64), 
+	tel  varchar(15), 
+	experience int, 
+	primary key(PK)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;
+  
+-- 建立 jspExercise 專案所需之表格
+DROP TABLE IF EXISTS  Member42;
+
+CREATE TABLE  Member42 (
+  memberId   	  VARCHAR(32) PRIMARY KEY NOT NULL,
+  PASSWORD   	  VARCHAR(32) NOT NULL,  
+  NAME            VARCHAR(32) NOT NULL,
+  address         VARCHAR(64),
+  phone           VARCHAR(15),
+  birthday        DATETIME,
+  registerdate 	  DATETIME NOT NULL DEFAULT NOW(),
+  picture         MEDIUMBLOB,
+  weight          FLOAT  
+) ENGINE=INNODB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;
+
+INSERT INTO Member42 (memberId, PASSWORD, NAME, address, phone, birthday,  registerdate, weight)
+VALUES ('mickey', '123', '米小薯', '台中市文心南路三段５８巷２２０號', '04-67891234', '1975-02-17', NOW(), 60);
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 jspExercise
+
+1.使用Eclipse編寫檔案，需安裝Tomcat9
+
+2.會使用到MySQL資料庫，建立練習用表格的檔案為CreateTableMySQL.sql
+
+3.需要修改資料庫密碼的檔案在\jspExercise\src\main\webapp\META-INF\context.xml
+	的這一段 password="Do!ng123"
